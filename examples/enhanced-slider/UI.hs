a0453ff2deeefbeebd06e3cbf42be090c07b7619
deltaspace0 <rgadeew@yandex.ru>
2023-01-20 18:29:43 +0300

    add onChangeReq to EnhancedSliderCfg

--- examples/enhanced-slider/UI.hs
+++ examples/enhanced-slider/UI.hs
@@ -10,7 +10,16 @@ import Monomer.EnhancedSlider
 import Model
 
 buildUI :: UIBuilder AppModel AppEvent
-buildUI _ _ = enhancedSlider_ number 0 50
-    [ titleCaption "Current value"
-    , dragRate 0.5
-    ]
+buildUI _ _ = tree where
+    tree = vstack_ [childSpacing_ 16]
+        [ enhancedSlider_ number 0 50
+            [ titleCaption "Current value"
+            , dragRate 0.5
+            , onChange fn
+            ]
+        , enhancedSlider_ changes 0 1000
+            [ titleCaption "Changes"
+            ]
+        ]
+    fn :: Double -> AppEvent
+    fn = const AppIncrementChanges
--- src/Monomer/EnhancedSlider/EnhancedSliderCfg.hs
+++ src/Monomer/EnhancedSlider/EnhancedSliderCfg.hs
@@ -13,18 +13,21 @@ import Monomer.Widgets.Single
 data EnhancedSliderCfg s e a = EnhancedSliderCfg
     { _escDragRate :: Maybe Rational
     , _escTitle :: Maybe Text
+    , _escOnChangeReq :: [a -> WidgetRequest s e]
     }
 
 instance Default (EnhancedSliderCfg s e a) where
     def = EnhancedSliderCfg
         { _escDragRate = Nothing
         , _escTitle = Nothing
+        , _escOnChangeReq = []
         }
 
 instance Semigroup (EnhancedSliderCfg s e a) where
     (<>) c1 c2 = EnhancedSliderCfg
         { _escDragRate = _escDragRate c1 <|> _escDragRate c2
         , _escTitle = _escTitle c1 <|> _escTitle c2
+        , _escOnChangeReq = _escOnChangeReq c1 <> _escOnChangeReq c2
         }
 
 instance Monoid (EnhancedSliderCfg s e a) where
@@ -38,4 +41,15 @@ instance CmbDragRate (EnhancedSliderCfg s e a) Rational where
 instance CmbTitleCaption (EnhancedSliderCfg s e a) where
     titleCaption title = def
         { _escTitle = Just title
+        }
+
+instance WidgetEvent e =>
+    CmbOnChange (EnhancedSliderCfg s e a) a e where
+        onChange fn = def
+            { _escOnChangeReq = [RaiseEvent . fn]
+            }
+
+instance CmbOnChangeReq (EnhancedSliderCfg s e a) s e a where
+    onChangeReq req = def
+        { _escOnChangeReq = [req]
         }
