f7d1573458527248c4964726d56739b0d6a8d153
deltaspace0 <rgadeew@yandex.ru>
2023-01-24 16:12:35 +0300

    add more tests for EnhancedSlider's buttons

--- test/Monomer/EnhancedSliderSpec.hs
+++ test/Monomer/EnhancedSliderSpec.hs
@@ -31,13 +31,32 @@ spec = describe "EnhancedSlider" $ do
 
 buttons :: Spec
 buttons = describe "buttons" $ do
+    minusButton
+    plusButton
+
+minusButton :: Spec
+minusButton = describe "\"-\" button" $ do
     let wenv = mockWenvEvtUnit (TestModel 42)
+        wenvM = mockWenvEvtUnit (TestModel 0)
         node = enhancedSlider_ field 0 50 [dragRate 2]
-        model p = nodeHandleEventModel wenv [evtClick p] node
-    it "should decrease the value when clicked on \"-\" button" $
-        model (Point (640-32-32-1) 50) ^. field `shouldBe` 40
-    it "should increase the value when clicked on \"+\" button" $
-        model (Point (640-1) 50) ^. field `shouldBe` 44
+        p = Point (640-32-32-1) 50
+        model wenv' = nodeHandleEventModel wenv' [evtClick p] node
+    it "should decrease the value if it's not minimum" $
+        model wenv ^. field `shouldBe` 40
+    it "shouldn't decrease the value if it's minimum" $
+        model wenvM ^. field `shouldBe` 0
+
+plusButton :: Spec
+plusButton = describe "\"+\" button" $ do
+    let wenv = mockWenvEvtUnit (TestModel 42)
+        wenvM = mockWenvEvtUnit (TestModel 50)
+        node = enhancedSlider_ field 0 50 [dragRate 2]
+        p = Point (640-1) 50
+        model wenv' = nodeHandleEventModel wenv' [evtClick p] node
+    it "should increase the value if it's not maximum" $
+        model wenv ^. field `shouldBe` 44
+    it "shouldn't increase the value if it's maximum" $
+        model wenvM ^. field `shouldBe` 50
 
 handleEvent :: Spec
 handleEvent = describe "handleEvent" $ do
