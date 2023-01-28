505cfdd87fc9fc0695871d94782ccc81164a1d36
deltaspace0 <rgadeew@yandex.ru>
2023-01-28 20:59:31 +0300

    provide captionMethod in SaveManager's example

--- examples/save-manager/UI.hs
+++ examples/save-manager/UI.hs
@@ -6,12 +6,16 @@ import Control.Lens
 import Monomer
 import Monomer.SaveManager
 import TextShow
+import qualified Data.Text as T
 
 import Model
 
 buildUI :: UIBuilder AppModel AppEvent
 buildUI _ model = hstack_ [childSpacing_ 128]
-    [ saveManager saves
+    [ saveManager_ saves
+        [ captionMethod $
+            \x z -> (showt x) <> " -- " <> (T.pack $ show z)
+        ]
     , separatorLine
     , label $ "Value: " <> (showt $ model ^. saves . currentData)
     , button "Increase" AppIncrease
