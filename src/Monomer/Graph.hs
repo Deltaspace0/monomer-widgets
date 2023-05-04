24f8b823eb4124193d0a9b98b7a1c2f8704c64d4
deltaspace0 <rgadeew@yandex.ru>
2023-05-04 16:10:28 +0300

    put numbers on graph axis

--- src/Monomer/Graph.hs
+++ src/Monomer/Graph.hs
@@ -9,8 +9,10 @@ import Control.Monad
 import Data.Default
 import Data.Fixed
 import Data.Maybe
+import Data.Text (pack)
 import Monomer.Graphics.ColorTable
 import Monomer.Widgets.Single
+import Numeric
 import qualified Monomer.Lens as L
 
 import Monomer.Graph.GraphState
@@ -70,8 +72,7 @@ makeGraph points state = widget where
             | otherwise = 10*d
             where
                 l = 10**(mod' (logBase 10 x) 1)
-                d = 10**(-1-(fromIntegral fl))
-                fl = (floor $ logBase 10 x) :: Int
+                d = 10**(-1-(floor' $ logBase 10 x))
         Point tx ty = _gsTranslation state
         Point cx cy = _gsScale state
         Point mx0 my0 = fromJust mp
@@ -92,17 +93,29 @@ makeGraph points state = widget where
             (p1, p2) = (Point ox gy, Point ox (gy+gh))
             (p3, p4) = (Point gx oy, Point (gx+gw) oy)
             line a b w c = drawLine renderer a b w $ Just c
+            font = styleFont style
+            fsize = FontSize 16
+            printText p t = renderText renderer p font fsize def t
+            show' n = s where
+                s = if round' n == n
+                    then show $ (round n :: Int)
+                    else showFFloat Nothing r ""
+                r = (round' $ n*bn)/bn
+                bn = 1000000000
+            printN p = printText p . pack . show'
         drawRect renderer rect (Just white) Nothing
         saveContext renderer
         intersectScissor renderer rect
         line p1 p2 2 black
         line p3 p4 2 black
         let (sx, sy) = (64*cx*ux, 64*cy*uy)
-            p (x, y) = Point (64*cx*x+ox) (64*cy*(-y)+oy)
-            f (a, b) = line (p a) (p b) 2 red
-            l = length points
             verLine x = line (Point x gy) (Point x (gy+gh)) 1 black
             horLine y = line (Point gx y) (Point (gx+gw) y) 1 black
+            uxl x = fromIntegral $ length $ show' $ uy*x
+            clampX x = max (gx+8) $ min (gx+gw-8*(uxl x)-4) $ ox+8
+            clampY = max (gy+16) $ min (gy+gh-16) $ oy+16
+            verN x = printN (Point (ox+sx*x+4) clampY) $ ux*x
+            horN x = printN (Point (clampX x) (oy-sy*x)) $ uy*x
         drawInAlpha renderer 0.25 $ do
             forM_ [ox, ox-sx/vs..gx] verLine
             forM_ [ox, ox+sx/vs..(gx+gw)] verLine
@@ -113,5 +126,20 @@ makeGraph points state = widget where
             forM_ [ox, ox+sx..(gx+gw)] verLine
             forM_ [oy, oy-sy..gy] horLine
             forM_ [oy, oy+sy..(gy+gh)] horLine
-        when (l > 1) $ forM_ (zip points (tail points)) f
+        let p (x, y) = Point (64*cx*x+ox) (64*cy*(-y)+oy)
+            connect (a, b) = line (p a) (p b) 2 red
+            l = length points
+        when (l > 1) $ forM_ (zip points (tail points)) connect
+        setFillColor renderer black
+        drawInAlpha renderer 0.62 $ do
+            forM_ [0..100] verN
+            forM_ [-1,(-2)..(-100)] verN
+            forM_ [1..100] horN
+            forM_ [-1,(-2)..(-100)] horN
         restoreContext renderer
+
+    floor' :: Double -> Double
+    floor' x = fromIntegral $ (floor x :: Int)
+
+    round' :: Double -> Double
+    round' x = fromIntegral $ (round x :: Int)
